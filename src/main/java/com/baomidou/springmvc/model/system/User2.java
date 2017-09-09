bc5ec716cbe08b088f489a04fb5490e459d989d0
zxy <375287760@qq.com>
2017-09-09 10:23:44 +0800

    commit test

---
+++ src/main/java/com/baomidou/springmvc/model/system/User2.java
@@ -0,0 +1,34 @@
+package com.baomidou.springmvc.model.system;
+
+/**
+ * Created by zxy on 2017/9/9.
+ */
+public class User2 {
+    private String name;
+    private String age;
+    private String birthdate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
